884d9d32cdf1d334c793abe8b3a3773ed05d1c34
Vipin <vipin.vipin.g498>
2018-07-14 23:10:30 +0530

    converted to electron

--- app.js
+++ app.js
@@ -1,52 +1,50 @@
-var robot = require("robotjs");
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var path = require('path');
-var bodyParser = require('body-parser');
-var io = require('socket.io')(http);
-var size =robot.getScreenSize();
-let fs = require('fs')
-let Jimp = require('jimp')
+const robot = require("robotjs");
+const express = require('express');
+const eapp = express();
+const http = require('http').Server(eapp);
+const path = require('path');
+const io = require('socket.io')(http);
+const size =robot.getScreenSize();
 var coords;
-let size1 = 1366;
-let rimg = robot.screen.capture();
-let fpath = 'public/myfile.png'
+const electron = require("electron");
+var ip = require('ip');
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,'/public')));
-app.get('/', function(req, res){
+const {app,BrowserWindow, Menu, ipcMain} = electron;
+let mainWindow, newWindow;
+
+eapp.use(express.static(path.join(__dirname,'/public')));
+eapp.get('/', function(req, res){
   res.sendFile(path.join(__dirname,'./public/index.html'));
 });
+
+app.on('ready',function(){
+  mainWindow = new BrowserWindow({webSecurity: false});
+  mainWindow.loadFile(`index.html`);
+  ipcMain.on('ip', function(e,i){
+    mainWindow.webContents.send('ip',ip.address());
+  });
+  mainWindow.on('closed',function(){
+      app.quit()
+  });
+  mainWindow.on('close', function(){
+      newWindow = null;
+  })
+
+
 
 io.on('connection', function(socket){
       socket.on('movemouse', function(msg){
         coords = formatCoordinates(msg);
         robot.moveMouse(coords.x, coords.y);
-     
       });
-      
       socket.on('scroll', function(msg){
         let scoords = formatCoordinates(msg);
         robot.scrollMouse(scoords.x, scoords.y);
       });
       socket.on('click',function(msg){
-        // let jimg = new Jimp(size.width, size.height);
-        // for (var x=0; x<size.width; x++) {
-        //     for (var y=0; y<size.height; y++) {
-        //         var index = (y * rimg.byteWidth) + (x * rimg.bytesPerPixel);
-        //         var r = rimg.image[index];
-        //         var g = rimg.image[index+1];
-        //         var b = rimg.image[index+2];
-        //         var num = (r*256) + (g*256*256) + (b*256*256*256) + 255;
-        //         jimg.setPixelColor(num, x, y);
-        //     }
-        // }
-        // jimg.write(fpath)
         robot.mouseClick();
-     })
-
-  });
+      })
+});
 
 function formatCoordinates(msg){
         let valueX = size.width/msg.split('&')[2]
@@ -56,7 +54,8 @@ function formatCoordinates(msg){
         return {x:xa, y:yb};
 }
 
-  
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+})
+
